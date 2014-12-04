dbc3f55874f16708314cc6a5b03874cc48c469e5
pfullen <peter@fullencpa.com>
2014-12-04 12:12:43 -0500

    Copied Database files
    Copied Database files

---
+++ sql/deacon.sql
@@ -0,0 +1,48 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.9
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 04, 2014 at 06:07 PM
+-- Server version: 5.6.14
+-- PHP Version: 5.5.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `deacon`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `homily`
+--
+
+CREATE TABLE IF NOT EXISTS `homily` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `title` varchar(255) DEFAULT NULL,
+  `gospel` varchar(255) DEFAULT NULL,
+  `calendarYear` varchar(255) DEFAULT NULL,
+  `homily` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `homily`
+--
+
+INSERT INTO `homily` (`id`, `date`, `title`, `gospel`, `calendarYear`, `homily`) VALUES
+(1, '2014-12-04 05:00:00', 'Test Title', 'Mark', 'B', 'This is a Test Homily.\nIn the name of the Father\nThe Son\nThe Holy Spirit ');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
